c0b82f21de90f613318483a64dc82208c13f8abb
charles-prof <profcharles2020@gmail.com>
2026-07-08 05:04:16 +0530

    chore(release): bump version to v1.7.1 for vercel blob deployment

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const CURRENT_APP_VERSION = '1.5.4';
+export const BASE_PATH = ''; // Or '/Space-Clocker/' if hosting on gh-pages subdirectory
+export const CURRENT_APP_VERSION = '1.7.1';
 export const XP_PER_LEVEL = 1000;
