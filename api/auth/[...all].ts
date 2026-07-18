4e8aab4e134d4dfb08c096504d7b886c7747b13a
charles-prof <profcharles2020@gmail.com>
2026-07-18 16:37:41 +0530

    refactor(auth): remove debug wrapper from vercel endpoint

--- api/auth/[...all].ts
+++ api/auth/[...all].ts
@@ -1,7 +1,4 @@
 import { auth } from "../lib/auth.js";
 import { toNodeHandler } from "better-auth/node";
 
-export default function handler(req: any, res: any) {
-  console.log("BETTER AUTH HIT:", req.method, req.url);
-  return toNodeHandler(auth)(req, res);
-}
+export default toNodeHandler(auth);
