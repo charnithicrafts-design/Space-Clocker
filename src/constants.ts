46cef5c8300a665a58d7070d7283031cf73a601a
charles-prof <profcharles2020@gmail.com>
2026-07-20 10:49:31 +0530

    chore(release): v1.8.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_PATH = ''; // Or '/Space-Clocker/' if hosting on gh-pages subdirectory
-export const CURRENT_APP_VERSION = '1.7.3';
+export const CURRENT_APP_VERSION = '1.8.0';
 export const XP_PER_LEVEL = 1000;
 export const ORACLE_MODEL = 'gemini-1.5-pro';
